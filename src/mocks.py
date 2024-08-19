2ac748981d211950e9fb97b71fa82aa865994db0
baby-platom <platon.programmer@gmail.com>
2024-08-19 18:21:58 +0500

    Update: better `mock_get_media_queue` func

--- src/mocks.py
+++ src/mocks.py
@@ -3,18 +3,31 @@ from collections.abc import AsyncGenerator
 from pathlib import Path
 
 
-async def mock_get_media_queue() -> AsyncGenerator[asyncio.Queue[Path]]:
+async def mock_get_media_queue(
+    source_folder: Path = Path("samples"),
+    n_tasks: int | None = None,
+    period: float = 5,
+) -> AsyncGenerator[asyncio.Queue[Path]]:
     queue = asyncio.Queue()
 
-    source_folder = Path("samples")
-    files = [file_path for file_path in source_folder.rglob("*") if file_path.is_file()]
-
-    async def produce(n: int) -> None:
+    async def produce(file_path: Path) -> None:
         while True:
-            await queue.put(files[n])
-            await asyncio.sleep(5)
+            await queue.put(file_path)
+            await asyncio.sleep(period)
 
-    tasks = [asyncio.create_task(produce(0)), asyncio.create_task(produce(1))]
+    files = [file_path for file_path in source_folder.rglob("*") if file_path.is_file()]
+    if len(files) < n_tasks:
+        raise ValueError(
+            f"Requested n_tasks '{n_tasks}' is bigger than"
+            f"number of files '{len(files)}' in the source_folder {str(source_folder)}"
+        )
+
+    tasks: list[asyncio.Task] = []
+    for i in range(n_tasks):
+        file_path = files[i]
+        task = asyncio.create_task(produce(file_path))
+        tasks.append(task)
+
     yield queue
 
     for task in tasks:
